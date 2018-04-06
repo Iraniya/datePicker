ebdca234c5155d13e055115b463a89d0c559aedf
Iraniya Naynesh <iraniya@iraniya.local>
2018-04-06 14:55:20 +0530

    Updated

--- date/ViewController.swift
+++ date/ViewController.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let datePicker = UIDatePicker(frame: CGRect(x: 0, y: 100, width: self.view.frame.size.width, height: 100))
+        
+        datePicker.datePickerMode = .time // setting mode to timer so user can only pick time as you want
+        datePicker.minuteInterval = 30  // with interval of 30
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat =  "HH:mm"
+        
+        let min = dateFormatter.date(from: "9:00")      //createing min time
+        let max = dateFormatter.date(from: "21:00") //creating max time
+        datePicker.minimumDate = min  //setting min time to picker
+        datePicker.maximumDate = max  //setting max time to picker
+        
+        view.addSubview(datePicker)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
